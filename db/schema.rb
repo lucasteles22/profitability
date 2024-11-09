4a4cac31b2da6155d96b00bcbc0f71f77e91d118
Lucas Teles <lucasteles22@gmail.com>
2024-11-09 14:30:43 -0300

    add authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_09_033647) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_09_171053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -24,4 +24,25 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_09_033647) do
     t.decimal "unit_price", precision: 8, scale: 2, null: false
     t.decimal "total_value", precision: 8, scale: 2, null: false
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
