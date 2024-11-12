53caee6564b475e0166c58c1859b07b9d59a1d95
Lucas Teles <lucasteles22@gmail.com>
2024-11-11 23:19:38 -0300

    configure devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resource :registration, only: %i[new create]
+  devise_for :users,
+    path: "",
+    path_names: { sign_in: "login", sign_out: "logout", registration: "signup" },
+    controllers: { sessions: "users/sessions", registrations: "users/registrations" }
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
