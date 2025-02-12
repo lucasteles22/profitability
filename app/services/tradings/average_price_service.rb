4de4601296a601e3cd1f33bffd8b76df92947851
Lucas Teles <lucasteles22@gmail.com>
2025-02-12 19:49:24 -0300

    refactor: query to calculate average price

--- app/services/tradings/average_price_service.rb
+++ app/services/tradings/average_price_service.rb
@@ -4,10 +4,10 @@ class Tradings::AveragePriceService
   end
 
   def call
-    average_price = query { ActiveRecord::Base.connection.execute(query_avg_price) }
+    tradings = query { ActiveRecord::Base.connection.execute(query_trading) }
     stock_bonus = query { ActiveRecord::Base.connection.execute(query_stock_bonus) }
 
-    map_to_average_price(average_price.value, stock_bonus.value)
+    map_to_average_price(tradings.value, stock_bonus.value)
   end
 
   private
@@ -20,29 +20,27 @@ class Tradings::AveragePriceService
     end
   end
 
-  def query_avg_price
+  def query_trading
     query = <<-SQL
       WITH trading AS (
-        SELECT SUM(total_value) AS "total_value", SUM(quantity) AS "quantity", RTRIM(code, 'F') AS "code"
+        SELECT SUM(CASE WHEN kind ILIKE '%compra%' THEN total_value ELSE 0 END) AS "total_purchase_price",
+          SUM(CASE WHEN kind not ILIKE '%compra%' THEN total_value ELSE 0 END) AS "total_sale_price",
+          SUM(CASE WHEN kind ILIKE '%compra%' THEN quantity ELSE 0 END) AS "total_purchase_quantity",
+          SUM(CASE WHEN kind not ILIKE '%compra%' THEN quantity ELSE 0 END) AS "total_sale_quantity",
+          RTRIM(code, 'F') AS "code"
         FROM tradings
         WHERE user_id = #{@current_user.id}
-        AND kind ilike '%compra%'
-        GROUP BY quantity, RTRIM(code, 'F')
-      ),
-      position AS (
-        SELECT RTRIM(code, 'F') AS "code", SUM(CASE WHEN kind ILIKE '%compra%' THEN quantity ELSE -quantity END) AS "quantity"
-        FROM tradings
         GROUP BY quantity, RTRIM(code, 'F')
       )
-      SELECT trading.code AS "product", SUM(trading.total_value) AS "total_value", SUM(trading.quantity) AS quantity
+      SELECT trading.code AS "product",
+        SUM(trading.total_purchase_price) AS "total_purchase_price",
+        SUM(trading.total_sale_price) AS "total_sale_price",
+        SUM(trading.total_purchase_quantity) AS "total_purchase_quantity",
+        SUM(trading.total_sale_quantity) AS "total_sale_quantity",
+        (SUM(trading.total_purchase_price) / SUM(trading.total_purchase_quantity)) as "average_price"
       FROM trading
-      WHERE trading.code IN (
-        SELECT position.code
-        FROM position
-        GROUP BY position.code
-        HAVING SUM(position.quantity) > 0
-      )
       GROUP BY trading.code
+      HAVING (SUM(total_purchase_quantity) - SUM(total_sale_quantity)) > 0
       ORDER BY trading.code ASC;
     SQL
   end
@@ -64,15 +62,17 @@ class Tradings::AveragePriceService
     SQL
   end
 
-  def map_to_average_price(average_price, stock_bonus)
+  def map_to_average_price(tradings, stock_bonus)
     products = []
-    average_price&.each do |row|
+    tradings&.each do |row|
       code = row["product"]
       stock = stock_bonus&.find { |x| x["product"]&.start_with?(code) }
-      total_value = row["total_value"] + (stock.present? ? stock["total_value"] : 0)
-      quantity = row["quantity"] + (stock.present? ? stock["quantity"] : 0)
 
-      products << AveragePrice.new(code, (total_value / quantity), quantity.to_i)
+      total_cost_basis = row["total_purchase_price"] + (stock.present? ? stock["total_value"] : 0)
+      quantity = row["total_purchase_quantity"] + (stock.present? ? stock["quantity"] : 0)
+      position_quantity = quantity - row["total_sale_quantity"]
+
+      products << AveragePrice.new(code, total_cost_basis, quantity, position_quantity)
     end
 
     products
